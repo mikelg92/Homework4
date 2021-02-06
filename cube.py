e13c0c55bae227b8360035f58a138db0aee03208
Michael Gonzales <gonzmich@oregonstate.edu>
2021-02-05 19:38:37 -0800

    cube: error handle for neg numbers

--- cube.py
+++ cube.py
@@ -4,9 +4,36 @@
 #l = length
 #w = width
 
-h = float(input("enter height: "))
-l = float(input("enter lenght: "))
-w = float(input("enter width: "))
+while True:
+    try:
+        h = float(input("enter height: "))
+        if h < 0:
+            while True:
+                h = float(input("invalid number, enter height: "))
+                if h < 0:
+                    True
+                else:
+                    break
+        l = float(input("enter lenght: "))
+        if l < 0:
+            while True:
+                l = float(input("invalid number, enter length: "))
+                if l < 0:
+                    True
+                else:
+                    break
+        w = float(input("enter width: "))
+        if w < 0:
+            while True:
+                w = float(input("invalid number, enter length: "))
+                if w < 0:
+                    True
+                else:
+                    break
+        break
+    except ValueError:
+        print("invalid number, only positive numbers.")
+
 
 volume = h*l*w
 
